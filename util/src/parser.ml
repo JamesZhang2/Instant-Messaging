c31184ae438b8f6284d05cb8f6e7fbc4a1667cf7
JamesZhang2 <jameszhang2019@outlook.com>
2022-03-10 20:41:32 -0500

    [WIP] Restructure parser, Parser.parse not implemented

--- util/src/parser.ml
+++ util/src/parser.ml
@@ -1,15 +1,17 @@
 open Yojson.Basic
 
 type pkt_type =
-  | Message
-  | FriendReq
-  | Registration
+  | SendMessage of string * string (* Receiver, Message *)
+  | GetMessage
+  | Registration of string (* Password, (TODO: add public key later) *)
+  | Login of string (* Password *)
+  | FriendReq of string * string (* Receiver, Message *)
+  | FriendReqReply of bool * string (* Accept or Reject, Receiver *)
   | Error
 
 type t = {
   pkt_type : pkt_type;
   sender : string;
-  receiver : string option;
   time : string;
   body : string;
 }
@@ -17,6 +19,5 @@ type t = {
 let parse json = json |> from_string |> failwith "Unimplemented"
 let pkt_type pkt = pkt.pkt_type
 let sender pkt = pkt.sender
-let receiver pkt = pkt.receiver
 let time pkt = pkt.time
 let body pkt = pkt.body
--- util/src/parser.mli
+++ util/src/parser.mli
@@ -1,27 +1,32 @@
 (** This module parses a json string to an internal type that represents
-    a packet of information. *)
+    a packet of information. The client uses this module to pack
+    requests into json strings. The server uses this module to parse
+    json strings into packets. *)
 
 type t
 (** Represents a packet of information. *)
 
+exception SyntaxError of string
+
 type pkt_type =
-  | Message
-  | FriendReq
-  | Registration
+  | SendMessage of string * string (* Receiver, Message *)
+  | GetMessage
+  | Registration of string (* Password, (TODO: add public key later) *)
+  | Login of string (* Password *)
+  | FriendReq of string * string (* Receiver, Message *)
+  | FriendReqReply of bool * string (* Accept or Reject, Receiver *)
   | Error
+(* TODO: Do we need this? *)
 
 val parse : string -> t
-(** [parse json] is the packet of information parsed from [json]. *)
+(** [parse json] is the packet of information parsed from [json].
+    Raises: [SyntaxError] if [json] is Malformed. *)
 
 val pkt_type : t -> pkt_type
 (** [pkt_type pkt] is the type of the packet [pkt]. *)
 
 val sender : t -> string
 (** [sender pkt] is the user name that sent the packet [pkt]. *)
-
-val receiver : t -> string option
-(** [receiver pkt] is [Some user] if [user] is the user name of the
-    indended receiver, or [None] if there is no receiver. *)
 
 val time : t -> string
 (** [time pkt] is the time that [pkt] is sent. *)
