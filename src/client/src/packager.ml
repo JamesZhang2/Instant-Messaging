849a82d6ba0a98267439e20382a8755352eab79e
JamesZhang2 <jameszhang2019@outlook.com>
2022-03-15 19:40:32 -0400

    Implement Client.Packager, add tests

--- src/client/src/packager.ml
+++ src/client/src/packager.ml
@@ -1,12 +1,52 @@
-(** [pack_time ()] is the json string that encodes the current time. *)
-let pack_time () =
-  "\"time\": \"" ^ Util.Time.string_of_now ~local:true ^ "\""
+(** [pack prop value] is the json string with property [prop] and value
+    [value]. *)
+let pack prop value = Printf.sprintf "\"%s\": \"%s\"" prop value
 
-let pack_send_msg sender receiver msg = failwith "Unimplemented"
-let pack_get_msg sender = failwith "Unimplemented"
-let pack_register username password = failwith "Unimplemented"
-let pack_login username password = failwith "Unimplemented"
-let pack_friend_req sender receiver msg = failwith "Unimplemented"
+(** [pack_type t] is the json string that encodes the type of the
+    package. *)
+let pack_type t = pack "type" t
+
+(** [pack_sender sender] is the json string that encodes the sender. *)
+let pack_sender sender = pack "sender" sender
+
+(** [pack_time ()] is the json string that encodes the current time. *)
+let pack_time () = pack "time" (Util.Time.string_of_now ~local:true)
+
+(** [pack_receiver receiver] is the json string that encodes the
+    receiver. *)
+let pack_receiver receiver = pack "receiver" receiver
+
+let pack_send_msg sender receiver msg =
+  Printf.sprintf "{%s,%s,%s,%s,%s}"
+    (pack_type "SendMessage")
+    (pack_sender sender) (pack_time ())
+    (pack_receiver receiver)
+    (pack "message" msg)
+
+let pack_get_msg sender =
+  Printf.sprintf "{%s,%s,%s}"
+    (pack_type "GetMessage")
+    (pack_sender sender) (pack_time ())
+
+let pack_register username password =
+  Printf.sprintf "{%s,%s,%s,%s}" (pack_type "Register")
+    (pack_sender username) (pack_time ())
+    (pack "password" password)
+
+let pack_login username password =
+  Printf.sprintf "{%s,%s,%s,%s}" (pack_type "Login")
+    (pack_sender username) (pack_time ())
+    (pack "password" password)
+
+let pack_friend_req sender receiver msg =
+  Printf.sprintf "{%s,%s,%s,%s,%s}" (pack_type "FriendReq")
+    (pack_sender sender) (pack_time ())
+    (pack_receiver receiver)
+    (pack "message" msg)
 
 let pack_friend_req_reply sender receiver accepted =
-  failwith "Unimplemented"
+  Printf.sprintf "{%s,%s,%s,%s,%s}"
+    (pack_type "FriendReqReply")
+    (pack_sender sender) (pack_time ())
+    (pack_receiver receiver)
+    (Printf.sprintf "\"%s\": %B" "accepted" accepted)
--- src/client/test/main.ml
+++ src/client/test/main.ml
@@ -20,12 +20,8 @@ let network_tests = []
 
 (******************** Packager Tests ********************)
 
-(** Returns true if [time] is in the right time format and false
-    otherwise. The right time format is (h)h:mm:ss (m)m/(d)d/yyyy, where
-    the hour, month and day fields do not start with a 0 (unless hour is
-    0) while the minute and second fields must have exactly two digits.
-    The hour field must be between 0 and 23, inclusive. *)
-let chk_time time =
+(** The regular expression for correctly formatted time. *)
+let time_regex =
   (* TODO: This does not check invalid dates like 2/31/2022. *)
   let hr = "\\([1]?[0-9]\\|2[0-3]\\)" in
   let min = "\\([0-5][0-9]\\)" in
@@ -33,10 +29,15 @@ let chk_time time =
   let month = "\\([1-9]\\|1[0-2]\\)" in
   let day = "\\([1-9]\\|[1-2][0-9]\\|3[0-1]\\)" in
   let yr = "\\([1-9][0-9][0-9][0-9]\\)" in
-  Str.string_match
-    (Str.regexp
-       (Printf.sprintf "^%s:%s:%s %s/%s/%s$" hr min sec month day yr))
-    time 0
+  Printf.sprintf "%s:%s:%s %s/%s/%s" hr min sec month day yr
+
+(** Returns true if [time] is in the right time format and false
+    otherwise. The right time format is (h)h:mm:ss (m)m/(d)d/yyyy, where
+    the hour, month and day fields do not start with a 0 (unless hour is
+    0) while the minute and second fields must have exactly two digits.
+    The hour field must be between 0 and 23, inclusive. *)
+let chk_time time =
+  Str.string_match ("^" ^ time_regex ^ "$" |> Str.regexp) time 0
 
 let chk_time_test
     (name : string)
@@ -61,7 +62,28 @@ let chk_time_tests =
     chk_time_test "last day of a year" "8:00:00 12/31/2000" true;
   ]
 
-let packager_tests = chk_time_tests @ []
+(** [remove_time j] removes the value of the time field from the json
+    string [j]. *)
+let remove_time j =
+  Str.global_replace (time_regex |> Str.regexp) "<removed>" j
+
+(** [equal_ignore_time s1 s2] is true if s1 and s2 are equal with the
+    value of the time field removed. *)
+let equal_ignore_time s1 s2 = remove_time s1 = remove_time s2
+
+let send_msg_1 =
+  "{\"type\": \"SendMessage\",\"sender\": \"Alice\",\"time\": \
+   \"8:00:00 3/1/2022\",\"receiver\": \"Bob\",\"message\": \"Hello \
+   world!\"}"
+
+let packager_tests =
+  chk_time_tests
+  @ [
+      ( "packing SendMessage" >:: fun _ ->
+        assert_equal send_msg_1
+          (pack_send_msg "Alice" "Bob" "Hello world!")
+          ~cmp:equal_ignore_time ~printer:remove_time );
+    ]
 
 (******************** Parser Tests ********************)
 
