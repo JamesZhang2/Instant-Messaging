d37e17a4d9e434a48a93821b711715fc656c7fe3
JamesZhang2 <jameszhang2019@outlook.com>
2022-03-16 11:23:34 -0400

    Add headers in controller

--- src/client/src/controller.ml
+++ src/client/src/controller.ml
@@ -11,9 +11,14 @@ type msg = {
 
 exception IllegalResponse
 
+let header body =
+  [ ("content-length", body |> String.length |> string_of_int) ]
+
 let send_msg sender receiver msg =
   let msg = Packager.pack_send_msg sender receiver msg in
-  let raw_response = Network.request "Post" ~body:msg ~header:[] in
+  let raw_response =
+    Network.request "Post" ~body:msg ~header:(header msg)
+  in
   (* let raw_body = Network.response_body raw_response in *)
   let status = Network.status raw_response in
   (* let body = *)
@@ -33,7 +38,9 @@ let parser_msg_controller receiver msg =
 
 let get_msg receiver =
   let request = Packager.pack_get_msg receiver in
-  let raw_response = Network.request "Get" ~body:request ~header:[] in
+  let raw_response =
+    Network.request "Get" ~body:request ~header:(header request)
+  in
   let raw_body =
     match Network.response_body raw_response with
     | None -> "No Response Message"
@@ -47,13 +54,17 @@ let get_msg receiver =
 
 let register username password =
   let message = Packager.pack_register username password in
-  let request = Network.request "Post" ~body:message ~header:[] in
+  let request =
+    Network.request "Post" ~body:message ~header:(header message)
+  in
   let raw_response = Network.status request in
   raw_response / 100 = 2
 
 let login username password =
   let message = Packager.pack_login username password in
-  let request = Network.request "Post" ~body:message ~header:[] in
+  let request =
+    Network.request "Post" ~body:message ~header:(header message)
+  in
   let raw_response = Network.response_body request in
   match raw_response with
   | None -> (true, "")
@@ -65,7 +76,9 @@ let login username password =
 
 let friend_req sender receiver msg =
   let message = Packager.pack_friend_req sender receiver msg in
-  let request = Network.request "Post" ~body:message ~header:[] in
+  let request =
+    Network.request "Post" ~body:message ~header:(header message)
+  in
   let raw_response = Network.status request in
   raw_response / 100 = 2
 
@@ -73,6 +86,8 @@ let friend_req_reply sender receiver accepted =
   let message =
     Packager.pack_friend_req_reply sender receiver accepted
   in
-  let request = Network.request "Post" ~body:message ~header:[] in
+  let request =
+    Network.request "Post" ~body:message ~header:(header message)
+  in
   let status = Network.status request in
   status / 100 = 2
