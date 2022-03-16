048aae05e6f30c6a032676d719ee6fa7a386d440
angelac345 <angelachao345@gmail.com>
2022-03-16 00:53:49 -0400

    Client Response parser
    Parses a json from the server into message

--- src/client/src/parser.ml
+++ src/client/src/parser.ml
@@ -1,12 +1,54 @@
+open Yojson.Basic
+
+type msg = {
+  purpose : string;
+  sender : string;
+  time : string;
+  message : string;
+}
+
 type response_type =
-  | GetMethResponse of string
-  | PostMethResponse of {
-      sender : string;
-      time : string;
-      message : string;
-    }
+  | GetMethResponse of msg list
+  | PostMethResponse of string
+  | ErrorResponse of string
 
-type t
+type t = {
+  time : string;
+  response : response_type;
+}
 
-let parse = failwith "Unimplemented"
-let get_type = failwith "Unimplemented"
+let rec parse_messages msg_list =
+  match msg_list with
+  | [] -> []
+  | h :: t ->
+      let assoc = Yojson.Basic.Util.to_assoc h in
+      let purp = "purpose" in
+      let util x = List.assoc x assoc |> Yojson.Basic.Util.to_string in
+      let sender = util "sender" in
+      let time = util "time" in
+      let message = util "message" in
+      { purpose = purp; sender; time; message } :: parse_messages t
+
+let parse json =
+  let conversion = Yojson.Basic.from_string json in
+  let largest = Yojson.Basic.Util.to_assoc conversion in
+  let res_type =
+    largest |> List.assoc "type" |> Yojson.Basic.Util.to_string
+  in
+  let body_t = largest |> List.assoc "message" in
+  if res_type = "Post" then
+    let msg = Yojson.Basic.Util.to_string body_t in
+    { time = "unimplemented"; response = PostMethResponse msg }
+  else if res_type = "Get" then
+    let json_list = Yojson.Basic.Util.to_list body_t in
+    let body = parse_messages json_list in
+    { time = "Unimplemented"; response = GetMethResponse body }
+  else
+    let msg = Yojson.Basic.Util.to_string body_t in
+    { time = "unimplemented"; response = ErrorResponse msg }
+
+let get_type t = t.response
+let msg_type msg = msg.purpose
+let msg_sender msg = msg.sender
+let get_time t = t.time
+let msg_body msg = msg.message
--- src/client/src/parser.mli
+++ src/client/src/parser.mli
@@ -3,13 +3,15 @@
 type t
 (** Abstract type representing a reponse from *)
 
+type msg
+(** Represents one message entry that the server may send back as a list
+    per user request. *)
+
 type response_type =
   | GetMethResponse of string
-  | PostMethResponse of {
-      sender : string;
-      time : string;
-      message : string;
-    }  (** Represents the type of data that the response is for. *)
+  | PostMethResponse of msg list
+  | ErrorResponse of string
+      (** Represents the type of data that the response is for. *)
 
 val parse : string -> t
 (** [parse json] returns the representation of the message that the json
@@ -18,3 +20,18 @@ val parse : string -> t
 val get_type : t -> response_type
 (** [get_type parsert] returns the response type of the parsed
     representation [parsert]*)
+
+val msg_type : msg -> string
+(** [msg_type t] determines what type this message is. "friend request"
+    if a response to a friend request, "message" if a normal message,
+    "other" if any other type of message*)
+
+val msg_sender : msg -> string
+(** [msg_sender msg] determines which user is the message from*)
+
+val get_time : t -> string
+(** [msg_time msg] is the time that the message [msg] was sent by the
+    other user*)
+
+val msg_body : msg -> string
+(** [msg_body msg] is the actual body of the message [msg]*)
