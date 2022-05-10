f5f242f28c38384cba18f159a0588749e6c01bf9
JamesZhang2 <jameszhang2019@outlook.com>
2022-05-09 22:10:00 -0400

    [WIP] Try to change text of heading

--- src/main.ml
+++ src/main.ml
@@ -5,9 +5,12 @@ let js = Js.string
 let document = Html.window##.document
 
 let alter_text txt =
-  let main =
-    Js.Opt.get
-      (document##getElementById (js "something"))
-      (fun () -> assert false)
-  in
-  ()
+  let head = document##getElementById (js "something") in
+  let main = Js.Opt.get head (fun () -> assert false) in
+  main##.innerHTML := js txt
+
+let onload _ =
+  alter_text "Hi";
+  Js._false
+
+let () = Html.window##.onload := Html.handler onload
