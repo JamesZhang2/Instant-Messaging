288f92afa3fb119f73b4eac27e86962db8d6ecef
JamesZhang2 <jameszhang2019@outlook.com>
2022-05-09 21:24:01 -0400

    Add dune and MakeFIle

--- src/author.mli
+++
@@ -1,9 +0,0 @@
-(** CS 3110 Spring 2022 Final Project
-
-    @author Angela Chao (ac2323)
-    @author Stephanie Ma (ym363)
-    @author James Zhang (rz234) *)
-
-val hours_worked : int
-(** [hours_worked] is the total number of hours we worked on the
-    project. *)
