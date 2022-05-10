48891ff299242d8c7516857aa14b60f5f246b91d
JamesZhang2 <jameszhang2019@outlook.com>
2022-05-10 18:15:08 -0400

    Use tab buttons to hide or show elements

--- src/main.ml
+++ src/main.ml
@@ -25,8 +25,49 @@ let login () =
         log (get_username ());
         Js._true)
 
+type tab =
+  | TMsg
+  | TFriends
+  | TFriendReq
+
+let switch_tab_aux (tab : tab) : unit =
+  let msg_tab = Html.getElementById "sidebar-msg" in
+  let friends_tab = Html.getElementById "sidebar-friends" in
+  let fr_tab = Html.getElementById "sidebar-fr" in
+  match tab with
+  | TMsg ->
+      msg_tab##.style##.display := js "block";
+      friends_tab##.style##.display := js "none";
+      fr_tab##.style##.display := js "none"
+  | TFriends ->
+      msg_tab##.style##.display := js "none";
+      friends_tab##.style##.display := js "block";
+      fr_tab##.style##.display := js "none"
+  | TFriendReq ->
+      msg_tab##.style##.display := js "none";
+      friends_tab##.style##.display := js "none";
+      fr_tab##.style##.display := js "block"
+
+let switch_tab () =
+  let msg_btn = Html.getElementById "tab-msg-btn" in
+  let friends_btn = Html.getElementById "tab-friends-btn" in
+  let fr_btn = Html.getElementById "tab-fr-btn" in
+  msg_btn##.onclick :=
+    Html.handler (fun ev ->
+        switch_tab_aux TMsg;
+        Js._true);
+  friends_btn##.onclick :=
+    Html.handler (fun ev ->
+        switch_tab_aux TFriends;
+        Js._true);
+  fr_btn##.onclick :=
+    Html.handler (fun ev ->
+        switch_tab_aux TFriendReq;
+        Js._true)
+
 let onload _ =
   login ();
+  switch_tab ();
   Js._false
 
 let () = Html.window##.onload := Html.handler onload
