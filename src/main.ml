f483ee6079ba435da4d429cf262633df7819b7df
JamesZhang2 <jameszhang2019@outlook.com>
2022-05-10 17:51:11 -0400

    Read values from input boxes and respond to button click events

--- src/main.ml
+++ src/main.ml
@@ -1,20 +1,32 @@
+(* Adapted from
+   https://github.com/ocsigen/js_of_ocaml/blob/master/examples/minesweeper *)
+
 open Js_of_ocaml
 module Html = Dom_html
 
 let js = Js.string
 let document = Html.window##.document
+let log (txt : string) : unit = Firebug.console##log (js txt)
 
-let alter_text txt =
-  let head = document##getElementById (js "something") in
-  let main = Js.Opt.get head (fun () -> assert false) in
-  main##.innerHTML := js txt
+let alter_text elt_id txt =
+  let head = Html.getElementById elt_id in
+  head##.innerHTML := js txt
+
+let get_username () : string =
+  let username_box = Html.getElementById "username-box" in
+  Js.to_string (Js.Unsafe.coerce username_box)##.value
+
+let login () =
+  let login_btn = Html.getElementById "login-btn" in
+  login_btn##.onclick :=
+    Html.handler (fun ev ->
+        alter_text "current_user"
+          ("You are currently logged in as " ^ get_username ());
+        log (get_username ());
+        Js._true)
 
 let onload _ =
-  alter_text "Hi";
+  login ();
   Js._false
 
-let exporter () = Js.export "main" (onload ())
-
-let () =
-  exporter ();
-  Html.window##.onload := Html.handler onload
+let () = Html.window##.onload := Html.handler onload
