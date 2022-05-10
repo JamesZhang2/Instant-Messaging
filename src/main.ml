b2df487252bb8119711a8ba5718492e7e4c10d63
JamesZhang2 <jameszhang2019@outlook.com>
2022-05-10 19:04:13 -0400

    Add tab styles

--- src/main.ml
+++ src/main.ml
@@ -7,6 +7,10 @@ module Html = Dom_html
 let js = Js.string
 let document = Html.window##.document
 let log (txt : string) : unit = Firebug.console##log (js txt)
+let show_elt elt = elt##.style##.display := js "block"
+let hide_elt elt = elt##.style##.display := js "none"
+let login_window = Html.getElementById "login-window"
+let main_window = Html.getElementById "main-window"
 
 let alter_text elt_id txt =
   let head = Html.getElementById elt_id in
@@ -22,16 +26,15 @@ let login () =
     Html.handler (fun ev ->
         alter_text "current_user"
           ("You are currently logged in as " ^ get_username ());
-        log (get_username ());
-        Js._true)
+        (* hide_elt login_window; show_elt main_window; *)
+        (* TODO: showing the main window doesn't work: destroys the
+           styles*)
+        Js._false)
 
 type tab =
   | TMsg
   | TFriends
   | TFriendReq
-
-let show_tab tab = tab##.style##.display := js "block"
-let hide_tab tab = tab##.style##.display := js "none"
 
 let switch_tab_aux (tab : tab) : unit =
   let msg_tab = Html.getElementById "sidebar-msg" in
@@ -39,17 +42,17 @@ let switch_tab_aux (tab : tab) : unit =
   let fr_tab = Html.getElementById "sidebar-fr" in
   match tab with
   | TMsg ->
-      show_tab msg_tab;
-      hide_tab friends_tab;
-      hide_tab fr_tab
+      show_elt msg_tab;
+      hide_elt friends_tab;
+      hide_elt fr_tab
   | TFriends ->
-      hide_tab msg_tab;
-      show_tab friends_tab;
-      hide_tab fr_tab
+      hide_elt msg_tab;
+      show_elt friends_tab;
+      hide_elt fr_tab
   | TFriendReq ->
-      hide_tab msg_tab;
-      hide_tab friends_tab;
-      show_tab fr_tab
+      hide_elt msg_tab;
+      hide_elt friends_tab;
+      show_elt fr_tab
 
 let tab_btn_event btn tab =
   btn##.onclick :=
