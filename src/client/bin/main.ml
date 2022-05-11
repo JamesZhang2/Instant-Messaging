e40e12dfa2366be0009c549c0f036a32ddd1c518
JamesZhang2 <jameszhang2019@outlook.com>
2022-05-11 16:15:48 -0400

    Test whether network is compatible with js_of_ocaml

--- src/client/bin/main.ml
+++ src/client/bin/main.ml
@@ -1,3 +1,8 @@
-open Client.Network
+(* Adapted from
+   https://github.com/ocsigen/js_of_ocaml/blob/master/examples/minesweeper *)
 
-let () = Client.Interface.run ()
+open Js_of_ocaml
+module Html = Dom_html
+
+let onload _ = Js._false
+let () = Html.window##.onload := Html.handler onload
--- src/client/src/network.ml
+++ src/client/src/network.ml
@@ -1,4 +1,3 @@
-open Piaf
 open Yojson.Basic.Util
 open Lwt
 open Cohttp
