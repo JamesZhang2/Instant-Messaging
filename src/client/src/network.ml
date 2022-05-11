2733a3eb4c6a31868a880adb3bf6caa0cb75439c
calciiium <calciiium.ma@gmail.com>
2022-05-11 03:46:11 -0400

    Implement client/network.ml using cohttp

--- src/client/src/network.ml
+++ src/client/src/network.ml
@@ -1,11 +1,11 @@
 open Piaf
 open Yojson.Basic.Util
+open Lwt
+open Cohttp
+open Cohttp_lwt
+open Cohttp_lwt_unix
 
-type t = {
-  status : int;
-  body : string option;
-  header : (string * string) list option;
-}
+type t = Cohttp_lwt.Response.t * Cohttp_lwt.Body.t
 
 exception RequestFailed of string
 exception IncorrectMeth
@@ -13,45 +13,46 @@ exception IncorrectMeth
 let url = "http://localhost:3000/"
 
 (** [get_body res] is the body of [res] with type [string]. *)
-let get_body (response : Response.t) =
-  let open Lwt_result.Syntax in
-  let body = Lwt_main.run (Body.to_string response.body) in
-  match body with
-  | Ok b -> b
-  | Error e -> raise (RequestFailed (Error.to_string e))
-
-(** [get_response res] is the response [res] in type [t]. *)
-let get_response (response : (Response.t, Error.t) Lwt_result.t) =
-  let res = Lwt_main.run response in
-  match res with
-  | Ok res ->
-      {
-        body = Some (res |> get_body);
-        status = Status.to_code res.status;
-        header = Some (Headers.to_list res.headers);
-      }
-  | Error e -> raise (RequestFailed (Error.to_string e))
+(* let get_body (response : Response.t) = let open Lwt_result.Syntax in
+   let body = Lwt_main.run (Body.to_string response.body) in match body
+   with | Ok b -> b | Error e -> raise (RequestFailed (Error.to_string
+   e)) (** [get_response res] is the response [res] in type [t]. *) let
+   get_response (response : (Response.t, Error.t) Lwt_result.t) = let
+   res = Lwt_main.run response in match res with | Ok res -> { body =
+   Some (res |> get_body); status = Status.to_code res.status; header =
+   Some (Headers.to_list res.headers); } | Error e -> raise
+   (RequestFailed (Error.to_string e)) *)
 
 let request ?(header = [ ("", "") ]) ?(body = "") (meth : string) : t =
   match meth with
   | "POST" ->
-      let res =
-        if header = [ ("", "") ] then
-          Piaf.Client.Oneshot.post ~body:(Body.of_string body)
-            (Uri.of_string (url ^ "post/"))
-        else
-          Piaf.Client.Oneshot.post ~headers:header
-            ~body:(Body.of_string body)
-            (Uri.of_string (url ^ "post/"))
+      let body =
+        Cohttp_lwt_unix.Client.post ~body:(Body.of_string body)
+          (Uri.of_string (url ^ "post/"))
       in
-      get_response res
+      Lwt_main.run body
+      (* let res = if header = [ ("", "") ] then
+         Piaf.Client.Oneshot.post ~body:(Body.of_string body)
+         (Uri.of_string (url ^ "post/")) else Piaf.Client.Oneshot.post
+         ~headers:header ~body:(Body.of_string body) (Uri.of_string (url
+         ^ "post/")) in get_response res *)
   | "GET" ->
-      let res =
-        Piaf.Client.Oneshot.get (Uri.of_string (url ^ "get/"))
+      let body =
+        Cohttp_lwt_unix.Client.get (Uri.of_string (url ^ "post/"))
       in
-      get_response res
+      Lwt_main.run body
   | _ -> raise IncorrectMeth
 
-let status res = res.status
-let response_body res = res.body
-let response_header res = res.header
+let status (res : t) =
+  match res with
+  | resp, _ -> resp |> Response.status |> Code.code_of_status
+
+let response_body (res : t) =
+  Some
+    (match res with
+    | _, body -> body |> Cohttp_lwt.Body.to_string |> Lwt_main.run)
+
+let response_header (res : t) =
+  Some
+    (match res with
+    | resp, _ -> resp |> Response.headers |> Header.to_list)
