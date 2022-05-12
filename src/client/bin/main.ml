697af550aa842b462699451f6d4a1ed8b907dde4
JamesZhang2 <jameszhang2019@outlook.com>
2022-05-11 21:45:45 -0400

    Revert client dune and main

--- src/client/bin/main.ml
+++ src/client/bin/main.ml
@@ -1,8 +1,3 @@
-(* Adapted from
-   https://github.com/ocsigen/js_of_ocaml/blob/master/examples/minesweeper *)
+open Client.Network
 
-open Js_of_ocaml
-module Html = Dom_html
-
-let onload _ = Js._false
-let () = Html.window##.onload := Html.handler onload
+let () = Client.Interface.run ()
