e99686df325c094a8f4071a18106eb1b10a3eb19
angelac345 <angelachao345@gmail.com>
2022-05-17 21:29:10 -0400

    Removing crypto and refactor

--- src/client/src/controller.ml
+++ src/client/src/controller.ml
@@ -8,9 +8,6 @@ open Util
     inclusive, and false otherwise. *)
 let is_successful status = status / 100 = 2
 
-(** It is the memory location that stores the current key of user*)
-let key_ref = ref (Crypto.sym_gen ())
-
 (** memory documentation of the current user*)
 let username_ref = ref ""
 
@@ -21,27 +18,29 @@ exception IllegalResponse
 let header body =
   [ ("content-length", body |> String.length |> string_of_int) ]
 
-(** [option_unpack op] unpacks an option into its string*)
-let option_unpack op =
-  match op with
-  | None -> ""
-  | Some x -> x
+(** [network_unpack none_ret some_func netop] gets the body of a
+    returned [Network.t]. If there is a body, apply [some_func] to the
+    body and return the result, otherwise, return [none_ret]*)
+let network_unpack none_ret some_func netop =
+  match Network.response_body netop with
+  | None -> none_ret
+  | Some x -> some_func x
+
+(** [current_time ()] is the current time in string format*)
+let current_time () = Time.string_of_now true
 
 (** [bool_post_parse raw_response] converts a response [raw_response] to
     post request to a tuple. Returns [(true, msg)] if the request is
     successful, [(false, error_msg)] otherwise*)
 let bool_post_parse raw_response =
   let raw_body =
-    raw_response |> Network.response_body |> option_unpack
+    raw_response |> network_unpack "No Response" (fun x -> x)
   in
-  (* if not status then (false, "Network Error") else *)
   let body = Parser.parse raw_body in
   match Parser.get_type body with
   | ErrorResponse x -> (false, x)
   | GetMsgResponse x -> (false, "don't use bool_post_parse")
   | PostMethResponse x -> (true, x)
-(* let message = Parser.get_plain body in (Parser.get_type body,
-   message) *)
 
 (** [fetch_key user] fetches the key for [user] from server*)
 let fetch_key user =
@@ -51,13 +50,6 @@ let fetch_key user =
   if not fetch_success then (false, "Unable to find user " ^ user)
   else (true, key)
 
-(** [db_op meth input] does the database operation [meth] until a
-    success is returned. *)
-let db_op meth input =
-  let _ = meth input in
-  ()
-(* if success then () else print_endline ("Error" ^ resp) *)
-
 (** [send_msg_master receiver msg packer msg_type db_meth rel_checker]*)
 let send_msg_master receiver msg packer msg_type db_meth =
   let sender = !username_ref in
@@ -66,11 +58,9 @@ let send_msg_master receiver msg packer msg_type db_meth =
   let success, resp = bool_post_parse raw_response in
   if success then
     let message =
-      Msg.make_msg sender receiver
-        (Time.string_of_now true)
-        msg_type msg
+      Msg.make_msg sender receiver (current_time ()) msg_type msg
     in
-    db_op db_meth message
+    db_meth message |> ignore
   else ();
   (success, resp)
 
@@ -103,19 +93,20 @@ let msg_processor receiver msg =
   in
   (let msg_type = Msg.msg_type msg in
    match msg_type with
-   | Message -> db_op (add_msg receiver) decrypt
+   | Message -> add_msg receiver decrypt |> ignore
    | FriendReqRep (accepted, key) ->
        let from = Msg.sender msg in
-       db_op (update_request receiver from) accepted
+       update_request receiver from accepted |> ignore
    | FriendReq ->
        let sender = Msg.sender msg in
        let success, key = fetch_key sender in
        let add_key = if not success then None else Some key in
-       db_op (add_request receiver decrypt add_key) None
-   | GCMessage -> db_op (add_msg_to_gc !username_ref) msg
+       add_request receiver decrypt add_key None |> ignore
+   | GCMessage -> add_msg_to_gc !username_ref msg |> ignore
    | GCRequest -> failwith "GCRequest Shouldn't be received by a client"
    | GCReqRep b ->
-       if b then db_op (add_member_gc (Msg.sender msg)) receiver else ());
+       if b then add_member_gc (Msg.sender msg) receiver |> ignore
+       else ());
   decrypt
 
 let members_of_gc gcid is_command =
@@ -132,16 +123,13 @@ let members_of_gc gcid is_command =
     if not fetch_success then (false, [ " Fetch failed " ])
     else
       let total_lst = Parser.to_str_list lststring in
-      (if is_gc !username_ref gcid then
-       let local_lst = members_of_gc !username_ref gcid in
-       let add_to_local =
-         List.filter (fun x -> not (List.mem x local_lst)) total_lst
-       in
-       let _ = add_member_gc !username_ref gcid add_to_local in
-       ()
-      else
-        let _ = add_groupchat !username_ref gcid total_lst in
-        ());
+      if is_gc !username_ref gcid then
+        let local_lst = members_of_gc !username_ref gcid in
+        let add_to_local =
+          List.filter (fun x -> not (List.mem x local_lst)) total_lst
+        in
+        add_member_gc !username_ref gcid add_to_local |> ignore
+      else add_groupchat !username_ref gcid total_lst |> ignore;
       (true, total_lst)
 
 (** [update_member_of_gc gcid] updates the local groupchat [gcid] with
@@ -156,14 +144,12 @@ let update_msg ?(amount = "unread") () =
     let receiver = !username_ref in
     let request = Packager.pack_get_msg receiver amount in
     let raw_response = Network.request "POST" ~body:request in
-    let raw_body =
-      match Network.response_body raw_response with
-      | None -> "No Response Message"
-      | Some msg -> msg
+    let body_type =
+      raw_response
+      |> network_unpack "No Response" (fun x -> x)
+      |> Parser.parse |> Parser.get_type
     in
-    (* let _ = print_endline raw_body in *)
-    let body = Parser.parse raw_body in
-    match Parser.get_type body with
+    match body_type with
     | ErrorResponse x -> (false, [])
     | PostMethResponse x -> raise IllegalResponse
     | GetMsgResponse lst ->
@@ -172,45 +158,41 @@ let update_msg ?(amount = "unread") () =
         (true, new_lst)
 
 let register username password =
-  let crypto = Util.Crypto.sym_gen () in
-  let key = crypto |> Util.Crypto.get_pub_str in
-  let message = Packager.pack_register username password key in
+  let message = Packager.pack_register username password "" in
   let raw_response = Network.request "POST" ~body:message in
   let successful, resp = bool_post_parse raw_response in
-  if successful then (
-    let _ = key_ref := crypto in
-    username_ref := username;
-    db_op init_dbs ();
-    db_op (create_dbs username) (Crypto.get_pub_str crypto))
+  if successful then
+    (username_ref := username;
+     init_dbs () |> ignore;
+     create_dbs username "")
+    |> ignore
   else ();
   (successful, resp)
 
 let login username password =
   let message = Packager.pack_login username password in
   let raw_response = Network.request "POST" ~body:message in
-  let raw_body = Network.response_body raw_response in
-  match raw_body with
-  | None -> (true, [])
-  | Some raw_body' -> (
-      match raw_body' |> Parser.parse |> Parser.get_type with
-      | ErrorResponse x -> (false, [ Msg.make_msg "" "" "" Message x ])
-      | GetMsgResponse x -> raise IllegalResponse
-      | PostMethResponse x ->
-          key_ref := Crypto.pub_from_str x;
-          username_ref := username;
-          db_op init_dbs ();
-          db_op (create_dbs username) (Crypto.get_pub_str !key_ref);
-          (* update messages*)
-          let success, messages =
-            if is_client username then update_msg ()
-            else update_msg ~amount:"2022-03-29 17:00:00" ()
-          in
-          (*update gc members*)
-          let _ = List.iter update_member_of_gc (gc_of_user username) in
-          let login_notification =
-            Msg.make_msg "" "" "" Message "Login Successful"
-          in
-          (success, login_notification :: messages))
+  let process_func raw_body' =
+    match raw_body' |> Parser.parse |> Parser.get_type with
+    | ErrorResponse x -> (false, [ Msg.make_msg "" "" "" Message x ])
+    | GetMsgResponse x -> raise IllegalResponse
+    | PostMethResponse x ->
+        username_ref := username;
+        init_dbs () |> ignore;
+        create_dbs username "" |> ignore;
+        (* update messages*)
+        let success, messages =
+          if is_client username then update_msg ()
+          else update_msg ~amount:"2022-03-29 17:00:00" ()
+        in
+        (*update gc members*)
+        List.iter update_member_of_gc (gc_of_user username);
+        let login_notification =
+          Msg.make_msg "" "" "" Message "Login Successful"
+        in
+        (success, login_notification :: messages)
+  in
+  raw_response |> network_unpack (true, []) process_func
 
 let logout () =
   username_ref := "";
@@ -218,10 +200,7 @@ let logout () =
 
 let friend_req receiver msg =
   if "" = !username_ref then (false, "User Not Logged in")
-  else if !username_ref = receiver then
-    (false, "Cannot send friend request to yourself")
-    (* else if isFriend !username_ref receiver then (true, "Already
-       Friends") *)
+  else if !username_ref = receiver then (false, "Friend Request to self")
   else
     let fetch_success, key = fetch_key receiver in
     if not fetch_success then (false, "Unable to find user " ^ receiver)
@@ -231,17 +210,13 @@ let friend_req receiver msg =
       let raw_response = Network.request "POST" ~body:message in
       let success, resp = bool_post_parse raw_response in
       let req =
-        Msg.make_msg sender receiver
-          (Time.string_of_now true)
-          FriendReq msg
+        Msg.make_msg sender receiver (current_time ()) FriendReq msg
       in
-      let _ =
-        if success then
-          let _ = add_request sender req (Some key) None in
-          ()
-        else ()
-      in
-      let _ = update_msg () in
+      (* add_friend request to database*)
+      if success then add_request sender req (Some key) None |> ignore
+      else ();
+      (* updates messages*)
+      update_msg () |> ignore;
       (success, resp)
 
 let friend_req_reply receiver accepted =
@@ -255,14 +230,9 @@ let friend_req_reply receiver accepted =
     in
     let raw_response = Network.request "POST" ~body:message in
     let successful, resp = bool_post_parse raw_response in
-    if successful then
-      if accepted then
-        let _ = update_request sender receiver true in
-        (successful, resp)
-      else
-        let _ = update_request sender receiver false in
-        (successful, resp)
-    else (successful, resp)
+    if successful then update_request sender receiver accepted |> ignore
+    else ();
+    (successful, resp)
 
 let join_gc gc password =
   if "" = !username_ref then (false, "User not logged in")
@@ -279,10 +249,18 @@ let join_gc gc password =
       (successful, "You have successfully joined the groupchat")
     else (successful, resp)
 
+(** [incorrect_usermsg] is the returner when if a user is incorrect*)
 let incorrect_usermsg =
   [
     Msg.make_msg "server" !username_ref "2022-03-29 17:00:00" Message
       "Incorrect User";
+  ]
+
+(** [not_friends_msg] is the returner if two clients are not friends*)
+let not_friends_msg =
+  [
+    Msg.make_msg "friend check" !username_ref "2022-03-29 17:00:00"
+      Message "Not Friends";
   ]
 
 let read_msg () =
@@ -294,6 +272,7 @@ let read_msg () =
 
 let read_msg_from sender =
   if "" = !username_ref then (false, incorrect_usermsg)
+  else if not (is_frd !username_ref sender) then (false, not_friends_msg)
   else
     let username = !username_ref in
     match
@@ -304,11 +283,7 @@ let read_msg_from sender =
 (**[invalid_gc_msg gc msg] is a error message using [msg] with
    associated with groupchat [gc]*)
 let invalid_gc_msg gc msg =
-  [
-    Msg.make_msg gc !username_ref
-      (Time.string_of_now true)
-      GCMessage msg;
-  ]
+  [ Msg.make_msg gc !username_ref (current_time ()) GCMessage msg ]
 
 let read_gc_msg gc =
   if "" = !username_ref then (false, incorrect_usermsg)
@@ -352,7 +327,7 @@ let create_groupchat id password =
     let successful, resp = bool_post_parse raw_response in
     if not successful then (false, resp)
     else
-      let _ = db_op (create_groupchat !username_ref) id in
+      let _ = create_groupchat !username_ref id in
       (successful, resp)
 
 let current_user () =
